c0a79996bae7cdec020808d7ef2cd8cccde424ef
TPL822_6 <tpl822_6@TPL822-6s-MBP.attlocal.net>
2024-08-28 18:44:58 -0700

    fetching data from open weather

---
+++ client/src/component/Search.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+
+// export default function SearchBox(){
+//     const [city,SetCity] = useState("");
+
+//     return (
+
+//         <div> 
+//             <label htmlFor="city">Enter a city:
+//                 <input type='text' id='city' name="city"/>
+//             </label>
+//             <button>Enter</button>
+//         </div>
+//     )
+// }
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) =>{
-    res.send("hello from home");
+
+// Allow all origins
+app.use(cors());
+
+
+//fetch data from openweatherapp
+app.get('/', (req, res) => {
+    fetch("https://samples.openweathermap.org/data/2.5/weather?q=${}&appid=b1b15e88fa797225412429c1c50c122a1")
+        .then(r => {
+            console.log(r);
+            if (r.ok)
+                return r.json();
+        })
+        .then(data => res.json(data));
 });
+
+
 
 
 app.listen(port, console.log(`server start at: locolhost:${port}`));
